2d752aa41e1eeafb8c7881e92954de8aaebd546c
gaddamanugu sri harika <151146980+GaddamanuguHarika@users.noreply.github.com>
2025-11-14 12:41:21 +0530

    Add C program file

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello from DevOps Assignment!\n");
+    printf("This is a C program added to the repository.\n");
+    printf("Repository: devops-assignment\n");
+    printf("Author: GaddamanuguHarika\n");
+    return 0;
+}
