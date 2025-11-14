04e83d32f8621604343273c233f19c727dd5a5dc
gaddamanugu sri harika <151146980+GaddamanuguHarika@users.noreply.github.com>
2025-11-14 12:55:09 +0530

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello from DevOps Assignment!\n");
-    printf("This is a C program added to the repository.\n");
-    printf("Repository: devops-assignment\n");
-    printf("Author: GaddamanuguHarika\n");
+    double num1, num2;
+    char op;
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+    printf("Enter an operator (+, -, *, /): ");
+    scanf(" %c", &op);
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch(op) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if(num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+        default:
+            printf("Invalid operator.\n");
+    }
     return 0;
 }
